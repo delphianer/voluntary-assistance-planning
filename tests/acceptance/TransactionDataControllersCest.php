661cee59ef953d2510d75139660e4e6f446c476d
delphianer <44456919+delphianer@users.noreply.github.com>
2020-07-11 20:44:14 +0200

    Added logon-routin for transactionData Process-Testing

--- tests/acceptance/TransactionDataControllersCest.php
+++ tests/acceptance/TransactionDataControllersCest.php
@@ -7,7 +7,7 @@ class TransactionDataControllersCest
      */
     private $cookie = null;
 
-    private $transactionDataTables = [
+    private $transactionDataModels = [
         'appointments',
     ];
 
@@ -23,10 +23,42 @@ class TransactionDataControllersCest
      */
     public function testIndexAsGuest(AcceptanceTester $I): void
     {
-        foreach ($this->transactionDataTables as $tbl) {
+        foreach ($this->transactionDataModels as $tbl) {
             $I->wantToTest("Dimension as Guest: ".$tbl);
             $I->amOnPage('/'.$tbl);
             $I->see('You don\'t have access to this module: private');
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function login(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/session/login');
+        $I->see('Log In');
+        $I->fillField('email', 'bob@phalcon.io');
+        $I->fillField('password', 'password1');
+        $I->click('//form/*[@type="submit"]');
+        $I->see('Search users');
+
+        $this->cookie = $I->grabCookie('PHPSESSID');
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testIndexAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->transactionDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl);
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('Search');
         }
     }
 
