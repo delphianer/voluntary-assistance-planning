8157c65054eae5d79058a8253d2e70dc4f583198
delphianer <44456919+delphianer@users.noreply.github.com>
2020-07-11 20:43:11 +0200

    prepared new acceptance test for appointments

---
+++ tests/acceptance/controllers/AppointmentsControllerCest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+namespace Vokuro\Tests\Acceptance\Controllers;
+
+use AcceptanceTester;
+
+final class AppointmentsControllerCest
+{
+    private $cookie = null;
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function login(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/session/login');
+        $I->see('Log In');
+        $I->fillField('email', 'bob@phalcon.io');
+        $I->fillField('password', 'password1');
+        $I->click('//form/*[@type="submit"]');
+        $I->see('Search users');
+
+        $this->cookie = $I->grabCookie('PHPSESSID');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testIndexAsGuest(AcceptanceTester $I): void
+    {
+        $I->amOnPage('/appointments');
+        $I->see('You don\'t have access to this module: private');
+    }
+
+    /**
+     * @depends login
+     * @param AcceptanceTester $I
+     */
+    public function testIndex(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/appointments');
+        $I->see('Search appointments');
+    }
+
+    /**
+     * @depends login
+     * @param AcceptanceTester $I
+     */
+    public function testSearch(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/appointments/search');
+        $I->see('Search result');
+    }
+
+    /**
+     * @depends login
+     * @param AcceptanceTester $I
+     */
+    public function testNew(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/appointments/new');
+        $I->see('Create appointments');
+    }
+
+    /**
+     * @depends login
+     * @param AcceptanceTester $I
+     */
+    public function testEdit(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/appointments/edit/1');
+        $I->see('Edit appointments');
+    }
+
+    /**
+     * @depends login
+     * @param AcceptanceTester $I
+     */
+    public function testDelete(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        // todo: implement: $I->amOnPage('/appointments/delete/4');
+    }
+}
