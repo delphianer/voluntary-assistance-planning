fe86e174ddf0420d81e95276a1d5394f16a3761d
delphianer <44456919+delphianer@users.noreply.github.com>
2020-07-21 07:04:45 +0200

    added another testcases

--- tests/acceptance/MasterDataControllerCest.php
+++ tests/acceptance/MasterDataControllerCest.php
@@ -12,6 +12,9 @@ final class MasterDataControllerCest
      * @var string|null
      */
     private $cookie = null;
+    private $fixTestLabel = 'automaticTestKey20200720original';
+    private $editedFixTestLabel = 'automaticTestKey20200720edited';
+    private $masterDataIDs = []; // then stop further tests at this controller
 
     private $masterDataModels = [
         'certificates',
@@ -21,7 +24,7 @@ final class MasterDataControllerCest
         'locations' ,
         'vehicleproperties',
         'vehicles' ,
-        'volunteers' ,
+        'volunteers',
         'volunteerscertificateslink' ,
     ];
 
@@ -58,6 +61,7 @@ final class MasterDataControllerCest
     }
 
 
+
     /**
      * @param AcceptanceTester $I
      */
@@ -75,10 +79,28 @@ final class MasterDataControllerCest
 
 
 
+    /* **************************************************************************************
+     *
+     * Tests if there is any php error message not seen yet
+     *
+     * ************************************************************************************** */
+
+
+
+    private function stdTestsForAllCommonActions(AcceptanceTester $I): void
+    {
+        $I->dontSee('Call Stack');
+        $I->dontSee(' on line ');
+        $I->dontSee('Fatal error:');
+        $I->dontSee(' Error ');
+        $I->dontSee('Uncaught ArgumentCountError');
+    }
+
+
     /**
      * @param AcceptanceTester $I
      */
-    public function testIndexAsAdminUser(AcceptanceTester $I): void
+    public function testIndexActionAsAdminUser(AcceptanceTester $I): void
     {
         $I->setCookie('PHPSESSID', $this->cookie);
 
@@ -87,12 +109,280 @@ final class MasterDataControllerCest
             $I->amOnPage('/'.$tbl);
             $I->dontSee('You don\'t have access to this module: private');
             $I->see('Search');
-            $I->amOnPage('/'.$tbl.'/new');
-            $I->dontSee('Call Stack');
-            $I->dontSee(' on line ');
-            $I->dontSee(' Error ');
+            $this->stdTestsForAllCommonActions($I);
         }
     }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testSearchActionAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl.'/search');
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('Search result');
+            $I->amOnPage('/'.$tbl.'/search');
+            $this->stdTestsForAllCommonActions($I);
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testNewActionAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl.'/new');
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('Create ');
+            $I->amOnPage('/'.$tbl.'/search');
+            $this->stdTestsForAllCommonActions($I);
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testCreateActionAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl.'/create');
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('Search ');
+            $this->stdTestsForAllCommonActions($I);
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testEditActionAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl.'/edit/-1');
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('was not found');
+            $this->stdTestsForAllCommonActions($I);
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testSaveActionAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl.'/save');
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('Search ');
+            $this->stdTestsForAllCommonActions($I);
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testDeleteActionAsAdminUser(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataModels as $tbl) {
+            $I->wantToTest("Dimension as AdminUser: ".$tbl);
+            $I->amOnPage('/'.$tbl.'/delete/-1');
+            $I->dontSee('You don\'t have access to this module: private');
+            $I->see('Search ');
+            $I->see('was not found');
+            $this->stdTestsForAllCommonActions($I);
+        }
+    }
+
+
+
+    /* **************************************************************************************
+     *
+     * Next Tests are connected between each other
+     *
+     * ************************************************************************************** */
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function testSearchActionNoExistingTestsYet(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->modelsWithDescriptionAndID as $tbl) {
+            $I->amOnPage('/'.$tbl.'/index');
+            $I->fillField('label', $this->fixTestLabel);
+            $I->click('//input[@type="submit"]');
+            $I->dontSee($this->fixTestLabel);
+            $I->amOnPage('/'.$tbl.'/index');
+            $I->fillField('label', $this->editedFixTestLabel);
+            $I->click('//input[@type="submit"]');
+            $I->dontSee($this->editedFixTestLabel);
+        }
+    }
+
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function newActionAndCreateActionAllMasterDataController(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->modelsWithDescriptionAndID as $tbl) {
+            $I->wantToTest("Create new ".$tbl);
+            $I->amOnPage('/'.$tbl.'/new');
+            $I->fillField('label', $this->fixTestLabel);
+            $I->seeInCurrentUrl('/'.$tbl.'/new');
+            $I->click('//input[@type="submit"]');
+            $I->seeInCurrentUrl('/'.$tbl.'/create');
+            $I->see('was created successfully');
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function searchActionAndExtractIdsAllMasterDataController(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->modelsWithDescriptionAndID as $tbl) {
+            $I->amOnPage('/'.$tbl.'/index');
+            $I->fillField('label', $this->fixTestLabel);
+            $I->click('//input[@type="submit"]');
+            $I->see($this->fixTestLabel);
+
+            // get edit-link and extract ID
+            $idStr = $I->grabAttributeFrom(Locator::combine('a[class="btn btn-sm btn-outline-warning"]', '//tbody/tr/td/a[0]'), 'href');
+            $idStrArr = explode('/', $idStr);
+            $this->masterDataIDs[$tbl] = $idStrArr[3];
+        }
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function searchNewCreatedAndEditThatOnAllMasterDataController(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->modelsWithDescriptionAndID as $tbl) {
+            $id = $this->masterDataIDs[$tbl];
+            $I->amOnPage('/'.$tbl.'/edit/'.$id);
+
+            $I->seeInField('label', $this->fixTestLabel);
+            $I->fillField('label', $this->editedFixTestLabel);
+            $I->click('//button[@type="submit"]');
+            $I->seeInCurrentUrl('/'.$tbl.'/save');
+            $I->see('was updated successfully');
+        }
+    }
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function oldTestLabelNotExistingAnymore(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataIDs as $key => $value) {
+            $I->amOnPage('/'.$key.'/index');
+            $I->fillField('id', $value);
+            $I->click('//input[@type="submit"]');
+            $I->seeInCurrentUrl('/'.$key.'/search');
+            $I->dontSee($this->fixTestLabel);
+        }
+    }
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function searchNewOneOnAllMasterDataController(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataIDs as $key => $value) {
+            $I->amOnPage('/'.$key.'/search/'.$value);
+            $I->dontSee($this->fixTestLabel);
+            $I->see($this->editedFixTestLabel);
+        }
+    }
+
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function deleteThatOnAllMasterDataController(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataIDs as $key => $value) {
+            $I->amOnPage('/'.$key.'/delete/'.$value);
+            $I->dontSee($this->fixTestLabel);
+            $I->dontSee($this->editedFixTestLabel);
+            $I->seeInCurrentUrl('/'.$key.'/delete/'.$value);
+        }
+    }
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function allTestLabelNotExistingAnymore(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        foreach ($this->masterDataIDs as $key => $value) {
+            $I->amOnPage('/'.$key.'/search/'.$value);
+            $I->dontSee($this->fixTestLabel);
+            $I->dontSee($this->editedFixTestLabel);
+        }
+    }
+
+
+
 
 
     /**
@@ -127,6 +417,8 @@ final class MasterDataControllerCest
             $idStrArr = explode('/', $idStr);
             $id = $idStrArr[3];
 
+            $I->wantToTest("Search new created ".$tbl);
+
             $I->wantToTest("edit new created (ID=".$id.") data at ".$tbl);
             $I->click(Locator::combine('a[class="btn btn-sm btn-outline-warning"]', '//tbody/tr/td/a[0]'));
             // stay edit:
@@ -147,16 +439,5 @@ final class MasterDataControllerCest
             $I->seeInCurrentUrl('/'.$tbl.'/delete/'.$id);
             $I->see('was deleted successfully');
         }
-        /* process-steps:
-            1. 'new', ok
-            2. 'create', ok
-            3. 'index', ok
-            4. 'search', ok
-            5. 'edit', ok
-            6. 'save', ok
-            7. 'search', ok
-            8. 'delete', ok
-        */
     }
-    // todo: create tests with tables without short_desc or ID
 }
--- tests/acceptance/TransactionDataControllersCest.php
+++ tests/acceptance/TransactionDataControllersCest.php
@@ -1,21 +1,41 @@
 <?php
 
+use Codeception\Util\Locator;
+
 class TransactionDataControllersCest
 {
     /**
      * @var string|null
      */
     private $cookie = null;
+    private $_operationId;
+    private $_operationShiftId;
+
+    private $operationUniqueData = [
+        'shortDescription' => 'Op!!KeepThemSave2020',
+        'longDescription' => 'just a description',
+        'longDescriptionEdited' => 'i edited this description, just to test it',
+        // let this default 'mainDepartmentId' => '',
+        'shift' => [
+            // operationId
+            'shortDescription' => 'first shift of Op!!KeepThemSave2020',
+            'longDescription' => 'just an other description',
+            'start' => '2021-01-01 00:00:00',
+            'end' => '2021-12-31 23:59:59',
+        ]
+    ];
 
     private $transactionDataModels = [
         'appointments',
         'operations',
         'operationshifts',
-        'OperationshiftsDepartmentsLink',
-        'OperationshiftsEquipmentLink',
-        'OperationshiftsVehiclesLink',
-        'OpshdeplVolunteersLink',
+        'operationshiftsdepartmentslink',
+        'operationshiftsequipmentlink',
+        'operationshiftsvehicleslink',
+        'opshdeplvolunteerslink',
     ];
+
+
 
 
 
@@ -69,7 +89,155 @@ class TransactionDataControllersCest
             $I->dontSee('Call Stack');
             $I->dontSee(' on line ');
             $I->dontSee(' Error ');
+            $I->dontSee('Fatal error:');
+            $I->dontSee('Uncaught ArgumentCountError');
         }
+    }
+
+
+
+
+
+    /* **************************************************************************************
+     *
+     * Standard process -> add operation, then 1 shift, then delete everything backwards
+     *
+     * ************************************************************************************** */
+
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function addAOperation(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/operations/new');
+        $I->fillField('shortDescription', $this->operationUniqueData['shortDescription']);
+        $I->fillField('longDescription', $this->operationUniqueData['longDescription']);
+        $I->click('//input[@type="submit"]');
+        $I->seeInCurrentUrl('/operations/create');
+        $I->see('operation was created successfully');
+    }
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function searchOperationAndGrapID(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/operations/index');
+        $I->fillField('shortDescription', $this->operationUniqueData['shortDescription']);
+        $I->click('//input[@type="submit"]');
+        $I->see($this->operationUniqueData['shortDescription']);
+        $I->see('Search result');
+
+        // get edit-link and extract ID
+        $idStr = $I->grabAttributeFrom(Locator::combine('a[class="btn btn-sm btn-outline-warning"]', '//tbody/tr/td/a[0]'), 'href');
+        $idStrArr = explode('/', $idStr);
+        $this->_operationId = $idStrArr[3];
+    }
+
+
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function editOperation(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/operations/edit/'.$this->_operationId);
+        $I->seeInField('shortDescription', $this->operationUniqueData['shortDescription']);
+        $I->seeInField('longDescription', $this->operationUniqueData['longDescription']);
+        $I->fillField('longDescription', $this->operationUniqueData['longDescriptionEdited']);
+        $I->click('//button[@type="submit"]');
+        $I->seeInCurrentUrl('/operations/save');
+        $I->see('operation was updated successfully');
+        $I->seeInField('longDescription', $this->operationUniqueData['longDescriptionEdited']);
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function editOperationAndAddShift(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/operations/edit/'.$this->_operationId);
+        $I->seeInField('shortDescription', $this->operationUniqueData['shortDescription']);
+        $I->click('//button[@type="submit"][@value="goToShift"]');
+        $I->see('Create Operation Shift');
+        $I->see('Add shift for Operation: '.$this->operationUniqueData['shortDescription']);
+        $I->fillField('shortDescription', $this->operationUniqueData['shift']['shortDescription']);
+        $I->fillField('longDescription', $this->operationUniqueData['shift']['longDescription']);
+        $I->fillField('//input[@name="start"]', $this->operationUniqueData['shift']['start']);
+        $I->fillField('//input[@name="end"]', $this->operationUniqueData['shift']['end']);
+        $I->click('//input[@type="submit"]');
+        $I->see('Edit operation');
+        $I->see($this->operationUniqueData['shift']['shortDescription']);
+
+        // get edit-link and extract ID
+        $idStr = $I->grabAttributeFrom('//button[@name="submitAction"][@class="btn btn-sm btn-outline-warning"]', 'value');
+        $this->_operationShiftId = preg_replace('/^edit/', '', $idStr);
+
+        /*
+         *
+                private $operationUniqueData = [
+                'shortDescription' => 'Op!!KeepThemSave2020',
+                'longDescription' => 'just a description',
+                'longDescriptionEdited' => 'i edited this description, just to test it',
+                // let this default 'mainDepartmentId' => '',
+                'shift' => [
+                    // operationId
+                    'shortDescription' => 'first shift of Op!!KeepThemSave2020',
+                    'longDescription' => 'just an other description',
+                    'start' => '2021-01-01 00:00:00',
+                    'end' => '2021-12-31 23:59:59',
+                ]
+            ];
+
+        // todo: add equipment, vehicle and a department
+        */
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function deleteOperationShift(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/operationshifts/search?id='.$this->_operationShiftId);
+        $I->see($this->operationUniqueData['shift']['shortDescription']);
+        $I->amOnPage('/operationshifts/delete/'.$this->_operationShiftId);
+        $I->see('was deleted successfully');
+        $I->amOnPage('/operationshifts/search?id='.$this->_operationShiftId);
+        $I->dontSee($this->operationUniqueData['shift']['shortDescription']);
+    }
+
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function deleteOperation(AcceptanceTester $I): void
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+
+        $I->amOnPage('/operations/search?id='.$this->_operationId);
+        $I->see($this->operationUniqueData['shortDescription']);
+        $I->amOnPage('/operations/delete/'.$this->_operationId);
+        $I->see('operation was deleted successfully');
+        $I->amOnPage('/operations/search?id='.$this->_operationId);
+        $I->dontSee($this->operationUniqueData['shortDescription']);
     }
 
 }
