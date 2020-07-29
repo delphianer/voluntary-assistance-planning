3ccb61ce070abfac6cd57250a4b5456eeb48e5e3
delphianer <44456919+delphianer@users.noreply.github.com>
2020-07-29 07:23:37 +0200

    add lastevent select

--- src/Controllers/EventlistController.php
+++ src/Controllers/EventlistController.php
@@ -45,6 +45,37 @@ class EventlistController extends ControllerBase
         return $results;
     }
 
+
+    public function getLastEventsSimpleFormat(int $limit)
+    {
+        $rawSQL = "
+            select 'ap' event_kind,
+                 a.id event_id,
+                 a.label event_label
+                ,a.start event_starting
+                ,a.end event_ending
+            from appointments a
+            where a.start < now()
+        union all
+            select 'op' event_kind
+                ,op.id event_id
+                ,op.shortDescription event_label
+                ,min(opsh.start) event_starting
+                ,min(opsh.end) event_ending
+            from operations op
+            inner join vokuro.operationshifts opsh on opsh.operationId = op.id
+            where opsh.`start` < now()
+            group by op.shortDescription
+        order by event_ending desc
+        limit $limit";
+
+        $db      = $this->di['db'];
+        $data    = $db->query($rawSQL);
+        $results = $data->fetchAll();
+
+        return $results;
+    }
+
     public function getSimpleCountFromTable(array $countColumns, array $fromTables, string $resultColName, string $whereCondition)
     {
         $model = $this
